a19c37ad466618ca11d606d31dd1b96fe82b237d
Kieter Philip Balisnomo <balisnom@ualberta.ca>
2016-09-27 17:42:47 -0600

    Made addTweet, hasTweet, and getTweet and tests for them.

---
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/TweetListLTest.java
@@ -0,0 +1,44 @@
+package ca.ualberta.cs.lonelytwitter;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+/**
+ * Created by kiete on 9/27/2016.
+ */
+
+public class TweetListLTest extends ActivityInstrumentationTestCase2<LonelyTwitterActivity> {
+    public TweetListLTest() {
+        super(LonelyTwitterActivity.class);
+    }
+
+    public void testAddTweet() {
+        TweetList list = new TweetList();
+        Tweet tweet = new NormalTweet("Hello!!!");
+        list.add(tweet);
+        assertTrue(false);
+        assertTrue(list.hasTweet(tweet));
+
+    }
+
+    public void testHasTweet() {
+        TweetList list = new TweetList();
+        Tweet tweet = new NormalTweet("Hello!!!");
+
+        assertFalse(list.hasTweet(tweet));
+        list.add(tweet);
+        assertTrue(list.hasTweet(tweet));
+    }
+
+    public void testGetTweet() {
+        TweetList list = new TweetList();
+
+        Tweet a = new NormalTweet("Hello!");
+        Tweet b = new NormalTweet("Hi");
+
+        list.add(a);
+        list.add(b);
+
+        assertEquals(a, list.getTweet(0));
+        assertEquals(b, list.getTweet(1));
+    }
+}
